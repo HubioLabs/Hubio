ebbfc02b0b0b7ee282b9ecda6494ae4da33c73cc
Maxence <maxence.cosaque@epita.fr>
2024-07-04 22:48:54 +0200

    feat: Adds the new app structure

---
+++ app/tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'tailwindcss';
+
+import { join } from 'path';
+import { skeleton } from '@skeletonlabs/skeleton/plugin';
+import * as themes from '@skeletonlabs/skeleton/themes';
+
+export default {
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		join(require.resolve('@skeletonlabs/skeleton-svelte'), '../**/*.{html,js,svelte,ts}')
+	],
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: [
+		skeleton({
+            themes: [ themes.cerberus, themes.rose ]
+        })
+	]
+} as Config;
