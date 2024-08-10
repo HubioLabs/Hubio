063f9b16a1fbf3eb00e535c5e0da40c19e973bdf
Maxence <maxence.cosaque@epita.fr>
2024-08-10 18:20:18 +0200

    feat: Adds the base window structure and controls

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -1,7 +1,57 @@
+use tauri::{App, Manager};
+use tauri_plugin_log::{Target, TargetKind};
+use tauri_plugin_structure_manager::StructureManagerExt;
+
+use window_vibrancy::apply_acrylic;
+
+use log::{info, trace};
+
+fn structure_verification(app: &mut App) -> Result<(), String> {
+    info!("Structure Verification ...");
+
+    app.verify_document()?;
+    trace!("Document OK");
+
+    info!("Structure Verification DONE");
+    Ok(())
+}
+
+fn window_setup(app: &mut App) {
+    info!("Window Setup ...");
+
+    let window = app.get_webview_window("main").unwrap();
+
+    apply_acrylic(&window, Some((18, 18, 18, 125)))
+        .expect("Unsupported platform! 'apply_acrylic' is only supported on Windows");
+
+    info!("Window Setup DONE");
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .targets([
+                    Target::new(TargetKind::Stdout),
+                    Target::new(TargetKind::LogDir { file_name: None }),
+                    Target::new(TargetKind::Webview),
+                ])
+                .build(),
+        )
         .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_structure_manager::init())
+        .setup(|app| {
+            info!("Hubio Setup ...");
+
+            structure_verification(app)?;
+
+            #[cfg(target_os = "windows")]
+            window_setup(app);
+
+            info!("Hubio Setup DONE\nRunning ...");
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,5 +1,83 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+	import { Maximize, Minimize, X, Minus } from 'lucide-svelte';
+	import { getCurrentWindow } from '@tauri-apps/api/window';
+	import { onMount } from 'svelte';
+	import { listen } from '@tauri-apps/api/event';
+
+	const appWindow = getCurrentWindow();
+
+	let fullscreen = false;
+	
+	onMount(async () => {
+		fullscreen = await appWindow.isMaximized();
+
+		// Listen for window resize events
+        await listen('tauri://resize', async () => {
+            fullscreen = await appWindow.isMaximized();
+        });
+	});
 </script>
 
-<slot></slot>
+<!-- The window -->
+<div class="flex flex-col w-full h-full bg-surface-950 bg-opacity-50">
+	<!-- Title bar with drag region -->
+	<div data-tauri-drag-region class="flex flex-row items-center h-10 pl-4 pr-4">
+		<!-- Title -->
+		<h1 class="text-xl mr-auto select-none">
+			Hubio
+		</h1>
+
+		<!-- Window controls -->
+		<div class="flex flex-row gap-6">
+			<!-- Minimize button -->
+			<button 
+				class="select-none hover:bg-surface-500 rounded-sm transition duration-200" 
+				on:click={async () => await appWindow.minimize()}
+			>
+				<Minus absoluteStrokeWidth size=20 />
+			</button>
+
+			{#if fullscreen}
+				<!-- Unfullscreen button -->
+				<button 
+					class="select-none hover:bg-surface-500 rounded-sm transition duration-200" 
+					on:click={async () => {
+							fullscreen = !fullscreen; 
+							await appWindow.toggleMaximize();
+						}
+					}
+				>
+					<Minimize absoluteStrokeWidth size=20 />
+				</button>
+			{:else}
+				<!-- Fullscreen button -->
+				<button 
+					class="select-none hover:bg-surface-500 rounded-sm transition duration-200" 
+					on:click={async () => {
+							fullscreen = !fullscreen; 
+							await appWindow.toggleMaximize();
+						}
+					}
+				>
+					<Maximize absoluteStrokeWidth size=20 />
+				</button>
+			{/if}
+
+			<!-- Close button -->
+			<button 
+				class="select-none hover:bg-error-500 rounded-sm transition duration-200" 
+				on:click={async () => await appWindow.close()}
+			>
+				<X absoluteStrokeWidth size=20 />
+			</button>
+		</div>
+	</div>
+
+	<!-- Main content -->
+	<div class="w-full h-full p-4 pt-0">
+		<main id="main" class="w-full h-full rounded-lg p-2 bg-surface-950 bg-opacity-50">
+			<slot />
+		</main>
+	</div>
+</div>
